768509b60ea392445dd3cd6f982230529531ccbb
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2019-03-23 11:59:57 +0900

    [update]1ゲーム中に複数ラウンドを繰り返すように修正

--- js/crocotyping.js
+++ js/crocotyping.js
@@ -1,43 +1,62 @@
-showTheGamePage();
 
-function showTheGamePage(){
-    
-    startRound();
+const interval = 5000;
+const maximumRound = 5;
+let roundCount = 0;
+let timeId;
+let nextCharacterNum = 1;
+let movingCrocodile;
+startGame();
+
+function startGame(){
+    const bd = document.querySelector('body');
+    bd.addEventListener('keyup',handleKeyup);
+    timeId = setInterval(execRound, interval);
 }
-function startRound(){
+function execRound(){
+    if(roundCount >= maximumRound){
+        clearInterval(timeId);
+        showGameResult();
+        return;
+    }
     setQuiz();
-    const movingCrocodile = startMovingCrocodile();
-    judge(movingCrocodile)
+    movingCrocodile = startMovingCrocodile();
+    roundCount++;
+}
+function showGameResult(){
+    alert("game over");
 }
 function setQuiz(){
     const tp = document.querySelector('#tp');
-    var quiz = `<table ><tr><td id='cur'>a</td><td>s</td><td>d</td><td>f</td></tr></table>`
+    const prevQuiz = document.querySelector('#quiz');
+    if(prevQuiz !== null){
+        document.querySelector('#quiz').remove(); 
+    }
+    const quiz = `<table id='quiz'><tbody><tr id='quizRow'><td id='td1'>a</td><td id='td2'>s</td><td id='td3'>d</td><td id='td4'>f</td></tr></tbody></table>`
     tp.insertAdjacentHTML('afterend',quiz);
+    nextCharacterNum = 1;
 }
-function judge(movingCrocodile){
-    const bd = document.querySelector('body');
-    bd.addEventListener('keyup',(event)=>{
-        const cur = document.querySelector('#cur');
-        let currentCharacter = cur.textContent;
-        if(event.key===currentCharacter){
-            cur.setAttribute('id','');
-            cur.style.color='lightgrey';
-            if(cur===cur.parentNode.lastElementChild){
-                movingCrocodile.pause();
-            }else{
-                cur.nextElementSibling.setAttribute('id','cur');
-            } 
-        }
-    })
+function handleKeyup(event){
+    const nextCharacter = document.querySelector('#td'+nextCharacterNum);
+    if(event.key===nextCharacter.textContent){
+        updateQuizPhraseState();
+    }
+    if(nextCharacter===document.querySelector('#quizRow').lastElementChild){
+        movingCrocodile.pause();
+    }
+}
+function updateQuizPhraseState(){
+    const nextCharacter = document.querySelector('#td'+nextCharacterNum);
+    nextCharacter.style.color='lightgrey';
+    nextCharacterNum++;
 }
 function startMovingCrocodile(){
     const elem = document.querySelector('#crocodile');
     const movingCrocodile = elem.animate(
         {
-            transform:['translateX(100px) rotate(0deg)','translateX(0px) rotate(0deg)']
+            transform:['translateX(200px) rotate(0deg)','translateX(0px) rotate(0deg)']
         },
         {
-            duration: 3000,
+            duration: interval,
         }
     );
     return movingCrocodile;
